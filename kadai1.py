fad9268ff1e9bf3b908bf536de2455edeb3eb5ba
Takeru Tsuchiya <aikonntatto@gmail.com>
2020-03-29 07:27:50 +0900

    first commit

---
+++ kadai1.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# ４次ルンゲクッタによって
+# dx/dy = -x^3 + sin(t)
+# をとく．
+
+def f(x, t):
+    return -x**3 + np.sin(t)
+
+a = 0.0
+b = 10.0
+N = 100
+h = (b-a)/N
+
+tpoints = np.arange(a, b, h)
+# print(tpoints)
+xpoints = []
+x = 0.0
+
+for t in tpoints:
+    xpoints.append(x)
+    k1 = h*f(x,t)
+    k2 = h*f(x+0.5*k1, t+0.5*h)
+    k3 = h*f(x+0.5*k2, t+0.5*h)
+    k4 = h*f(x+k3, t+h)
+    x += (k1+2*k2+2*k3+k4)/6
+
+plt.plot(tpoints, xpoints)
+plt.xlabel("t")
+plt.ylabel("x(t)")
+plt.show
+
